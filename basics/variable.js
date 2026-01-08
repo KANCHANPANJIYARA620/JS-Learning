7ebaa742cfbdde449bde808f88ce2f9468d64061
KANCHAN KUMARI <119803602+KANCHANPANJIYARA620@users.noreply.github.com>
2026-01-08 07:52:29 +0000

    variable

---
+++ basics/variable.js
@@ -0,0 +1,19 @@
+// variable and Constant in JS
+
+//It's Constant
+const userName = "Panjiyara"  
+//  userName = "Kanchan" // It's value can't change.
+console.log(userName);
+
+
+// variable 
+let userAddress = "Banka"
+var userPassword = "919900"
+userDream = "OwnBigHome"
+
+// change above Values
+userAddress = "Godda"
+userPassword = " 6203736"
+userDream = " BecomeSoftwareEngineer"
+
+console.table([userAddress,userPassword,userDream])
